72b42dea5aa0f1c58b070661abb4cc888faa0906
Isaac Grynsztein <IsaacMGrynsztein@gmail.com>
2020-03-25 07:39:39 -0400

    About dialog now informs user when update is available or when they are up to date based on the github release api

--- src/app/dialogs/about-dialog/about-dialog.component.ts
+++ src/app/dialogs/about-dialog/about-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'app/posts.services';
 
 @Component({
   selector: 'app-about-dialog',
@@ -10,12 +11,23 @@ export class AboutDialogComponent implements OnInit {
   projectLink = 'https://github.com/Tzahi12345/YoutubeDL-Material';
   issuesLink = 'https://github.com/Tzahi12345/YoutubeDL-Material/issues';
   latestUpdateLink = 'https://github.com/Tzahi12345/YoutubeDL-Material/releases/latest'
+  latestGithubRelease = null;
+  checking_for_updates = true;
 
-  version = 'v3.5';
+  current_version_tag = 'v3.5';
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.getLatestGithubRelease()
+  }
+
+  getLatestGithubRelease() {
+    this.postsService.getLatestGithubRelease().subscribe(res => {
+      this.checking_for_updates = false;
+      this.latestGithubRelease = res;
+      console.log(this.latestGithubRelease)
+    });
   }
 
 }
