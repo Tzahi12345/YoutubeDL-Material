5bfecfcefe3f0e9dc92185c2bfe64a1c5bcb27fb
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-06-03 01:35:08 +0200

    Run docker as non root, copy package-json.lock

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+CMD="node app.js"
+
+# if the first arg starts with "-" pass it to program
+if [ "${1#-}" != "$1" ]; then
+  set -- "$CMD" "$@"
+fi
+
+# chown current working directory to current user
+if [ "$@" = "$CMD" ] && [ "$(id -u)" = "0" ]; then
+  find . \! -user "$UID" -exec chown "$UID:$GID" -R '{}' +
+  exec su-exec "$UID:$GID" "$0" "$@"
+fi
+
+exec "$@"
