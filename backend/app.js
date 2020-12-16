e3374c573a5305d7ccec9fb65f5cccf731e1c226
Isaac Abadi <IsaacMGrynsztein@gmail.com>
2020-12-15 20:16:53 -0500

    Args incompatible with video mode and audio-only mode will now get removed

--- backend/app.js
+++ backend/app.js
@@ -1505,6 +1505,10 @@ async function generateArgs(url, type, options) {
         }
 
     }
+
+    // filter out incompatible args
+    downloadConfig = filterArgs(downloadConfig, is_audio);
+
     logger.verbose(`youtube-dl args being used: ${downloadConfig.join(',')}`);
     return downloadConfig;
 }
@@ -1533,6 +1537,13 @@ async function getVideoInfoByURL(url, args = [], download = null) {
             }
         });
     });
+}
+
+function filterArgs(args, isAudio) {
+    const video_only_args = ['--add-metadata', '--embed-subs', '--xattrs'];
+    const audio_only_args = ['-x', '--extract-audio', '--embed-thumbnail'];
+    const args_to_remove = isAudio ? video_only_args : audio_only_args;
+    return args.filter(x => !args_to_remove.includes(x));
 }
 
 // currently only works for single urls
