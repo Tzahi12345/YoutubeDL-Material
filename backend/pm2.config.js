8738b13cd11b625c11aea7d8a6b4440b6b7a9892
GlassedSilver <glassedsilver@gmail.com>
2022-05-05 00:38:40 +0200

    Usage of PM2 Cluster Mode to increase performance

--- backend/pm2.config.js
+++ backend/pm2.config.js
@@ -2,6 +2,8 @@ module.exports = {
     apps : [{
       name   : "YoutubeDL-Material",
       script : "./app.js",
+      instances: 0,
+      exec_mode: "cluster",
       watch  : "placeholder",
       out_file: "/dev/null",
       error_file: "/dev/null"
