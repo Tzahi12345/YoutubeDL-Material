feff8b246147cda61ee92372136990d709647c0d
Glassed Silver <falk993@gmx.net>
2022-05-05 18:24:06 +0200

    revert pm2 config cluster_mode
    at least for now, implementation needs more research

--- backend/pm2.config.js
+++ backend/pm2.config.js
@@ -2,8 +2,6 @@ module.exports = {
     apps : [{
       name   : "YoutubeDL-Material",
       script : "./app.js",
-      instances: 0,
-      exec_mode: "cluster",
       watch  : "placeholder",
       out_file: "/dev/null",
       error_file: "/dev/null"
