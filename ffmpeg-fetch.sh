a21dc85d154366fb80efdf98217714ae0bd0050f
Dedy Martadinata S <dedyms@pm.me>
2022-05-07 12:10:23 +0700

    revert script

--- ffmpeg-fetch.sh
+++ ffmpeg-fetch.sh
@@ -22,21 +22,20 @@ esac
 
 echo "(INFO) Architecture detected: $ARCH"
 echo "(1/5) READY - Acquire temp dependencies in ffmpeg obtain layer"
-apt update && apt install -y --no-install-recommends wget xz-utils ca-certificates
+apt-get update && apt-get -y install curl xz-utils
 echo "(2/5) DOWNLOAD - Acquire latest ffmpeg and ffprobe from John van Sickle's master-sourced builds in ffmpeg obtain layer"
-#curl -o ffmpeg.txz \
-#    --connect-timeout 5 \
-#    --max-time 10 \
-#    --retry 5 \
-#    --retry-delay 0 \
-#    --retry-max-time 40 \
-#    "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-${ARCH}-static.tar.xz"
-wget -O ffmpeg.txz -c -t 10 "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-${ARCH}-static.tar.xz"
+curl -o ffmpeg.txz \
+    --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-${ARCH}-static.tar.xz"
 mkdir /tmp/ffmpeg
 tar xf ffmpeg.txz -C /tmp/ffmpeg
 echo "(3/5) CLEANUP - Remove temp dependencies from ffmpeg obtain layer"
-apt -y remove curl xz-utils
-apt -y autoremove --purge
+apt-get -y remove curl xz-utils
+apt-get -y autoremove
 echo "(4/5) PROVISION - Provide ffmpeg and ffprobe from ffmpeg obtain layer"
 cp /tmp/ffmpeg/*/ffmpeg /usr/local/bin/ffmpeg
 cp /tmp/ffmpeg/*/ffprobe /usr/local/bin/ffprobe
