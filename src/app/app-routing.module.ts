6d5a108cb6142bd68f4e4c916c8c2c2f7e1c7a81
Isaac Abadi <IsaacMGrynsztein@gmail.com>
2022-11-24 15:58:31 -0500

    Updated angular to v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
