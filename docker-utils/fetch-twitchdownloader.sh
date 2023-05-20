2adbc0a02caa8d36d19260365559fc0185cdd970
Dedy Martadinata S <dedyms@proton.me>
2023-05-20 11:35:12 +0700

    Update fetch-twitchdownloader.sh

--- docker-utils/fetch-twitchdownloader.sh
+++ docker-utils/fetch-twitchdownloader.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+!/bin/bash -eu
 
 # THANK YOU TALULAH (https://github.com/nottalulah) for your help in figuring this out
 # and also optimizing some code with this commit.
@@ -22,7 +22,7 @@ esac
 
 echo "(INFO) Architecture detected: $ARCH"
 echo "(1/5) READY - Install unzip"
-apt-get update && apt-get -y install unzip curl jq libicu66
+apt-get update && apt-get -y install unzip curl jq libicu70
 VERSION=$(curl --silent "https://api.github.com/repos/lay295/TwitchDownloader/releases/latest" | jq -r .tag_name)
 echo "(2/5) DOWNLOAD - Acquire twitchdownloader"
 curl -o twitchdownloader.zip \
