ba0de7f95c265471c7f37af5e00c031dc8f7d3f6
Dedy Martadinata S <dedyms@proton.me>
2023-05-26 08:54:54 +0700

    Fix entrypoint.sh (#921)
    * Revert https://github.com/Tzahi12345/YoutubeDL-Material/pull/864
    
    add && make it err and skipping perm check
    
    * Restore armv7 nightly
    
    * Update entrypoint.sh
    
    * simplify, always setup permission regardless uid

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
-CMD="npm start && pm2 start"
-
-# if the first arg starts with "-" pass it to program
-if [ "${1#-}" != "$1" ]; then
-  set -- "$CMD" "$@"
-fi
-
 # chown current working directory to current user
-if [ "$*" = "$CMD" ] && [ "$(id -u)" = "0" ]; then
-  find . \! -user "$UID" -exec chown "$UID:$GID" -R '{}' + || echo "WARNING! Could not change directory ownership. If you manage permissions externally this is fine, otherwise you may experience issues when downloading or deleting videos."
-  exec gosu "$UID:$GID" "$0" "$@"
-fi
-
-exec "$@"
+echo "[entrypoint] setup permission, this may take a while"
+find . \! -user "$UID" -exec chown "$UID:$GID" '{}' + || echo "WARNING! Could not change directory ownership. If you manage permissions externally this is fine, otherwise you may experience issues when downloading or deleting videos."
+exec gosu "$UID:$GID" "$@"
